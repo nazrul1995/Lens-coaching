c56dd0ab952a83f3dc6b0c35caa95a4cb17a11b9
nazrul islam <nazrulislamtube@gmail.com>
2025-11-27 14:00:54 +0600

    api update

--- src/app/Firebase/firebase.config.js
+++ src/app/Firebase/firebase.config.js
@@ -3,13 +3,14 @@ import { getAuth } from "firebase/auth";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCKiTCQEvDrT2TC11cl8C-tpToLKg5mjBY",
-  authDomain: "kids-shop-121d0.firebaseapp.com",
-  projectId: "kids-shop-121d0",
-  storageBucket: "kids-shop-121d0.firebasestorage.app",
-  messagingSenderId: "457283364127",
-  appId: "1:457283364127:web:c28ef7a26929d1a57b3c2a"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
